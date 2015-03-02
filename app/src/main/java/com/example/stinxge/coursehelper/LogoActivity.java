d90d1a6d42ba636872c00e24807d3f0df4fa025b
sTinGe <stinge62580@gmail.com>
2015-03-02 10:00:41 +0800

    add logo & logoanimation

--- app/src/main/java/com/example/stinxge/coursehelper/LogoActivity.java
+++ app/src/main/java/com/example/stinxge/coursehelper/LogoActivity.java
@@ -1,9 +1,16 @@
 package com.example.stinxge.coursehelper;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 
 public class LogoActivity extends ActionBarActivity {
@@ -11,7 +18,35 @@ public class LogoActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.logo);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+        alphaAnimation.setDuration(1000);
+
+        ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent = new Intent(LogoActivity.this, MainActivity.class);
+                startActivity(intent);
+                LogoActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/stinxge/coursehelper/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.stinxge.coursehelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by stinxge on 2015/3/2.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.main);
+    }
+}
