d7c98f708d9aa7721b7db75141df996daf1c9bf8
sTinGe <stinge62580@gmail.com>
2015-03-02 19:39:14 +0800

    add course button & design

--- app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
+++ app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
@@ -8,8 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.stinxge.coursehelper.MainActivity;
 import com.example.stinxge.coursehelper.R;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 /**
  * Created by stinxge on 2015/3/2.
@@ -17,6 +20,16 @@ import com.example.stinxge.coursehelper.R;
 public class CourseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.course, container, false);
+        View view = inflater.inflate(R.layout.course, container, false);
+        FloatingActionButton button = (FloatingActionButton) view.findViewById(R.id.course_icon);
+        button.setIcon(R.drawable.course);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "click button", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return view;
     }
 }
