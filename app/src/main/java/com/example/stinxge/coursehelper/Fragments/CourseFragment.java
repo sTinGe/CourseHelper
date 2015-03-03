10d1456d3e777ea51b6209335e2a84bd37eb6e04
sTinGe <stinge62580@gmail.com>
2015-03-03 09:34:13 +0800

    update course icon animation

--- app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
+++ app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
@@ -1,6 +1,8 @@
 package com.example.stinxge.coursehelper.Fragments;
 
 import android.app.Fragment;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 import com.example.stinxge.coursehelper.MainActivity;
 import com.example.stinxge.coursehelper.R;
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 /**
  * Created by stinxge on 2015/3/2.
@@ -20,17 +23,6 @@ import com.getbase.floatingactionbutton.FloatingActionButton;
 public class CourseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.course, container, false);
-        FloatingActionButton button = (FloatingActionButton) view.findViewById(R.id.course_icon);
-        button.setIcon(R.drawable.course);
-        button.setStrokeVisible(false);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "click button", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.course, container, false);
     }
 }
