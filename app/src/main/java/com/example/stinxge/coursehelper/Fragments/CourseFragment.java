19303b1ea352f1e976412befae7bd8023878e9c5
sTinGe <stinge62580@gmail.com>
2015-03-04 00:24:34 +0800

    update course&note button animation

--- app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
+++ app/src/main/java/com/example/stinxge/coursehelper/Fragments/CourseFragment.java
@@ -1,21 +1,23 @@
 package com.example.stinxge.coursehelper.Fragments;
 
 import android.app.Fragment;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
+import android.drm.DrmManagerClient;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.stinxge.coursehelper.MainActivity;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
 import com.example.stinxge.coursehelper.R;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import java.util.List;
 
 /**
  * Created by stinxge on 2015/3/2.
@@ -24,6 +26,8 @@ public class CourseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View course = inflater.inflate(R.layout.course, container, false);
+
+        // Three behavior for WeekView
 
         final FloatingActionsMenu menu = (FloatingActionsMenu) course.findViewById(R.id.menu);
         final FloatingActionButton action1 = (FloatingActionButton) course.findViewById(R.id.action1);
@@ -45,4 +49,6 @@ public class CourseFragment extends Fragment {
 
         return course;
     }
+
+
 }
